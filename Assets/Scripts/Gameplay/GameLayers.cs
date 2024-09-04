950bd904a92ab9c99ccee47a4cb977dc888b3650
Gonçalo Oliveira <goncalo.27@hotmail.com>
2024-09-04 21:04:38 +0100

    Added Trainers - Implemented trainers FOV to detect the player; - Implemented trainer battle warning UI; - Implemented trainer cutscene that prevents the player to move while the trainer is getting to its position; - Implemented configurable trainer's facing direction that rotates the sprite and fov collider accordingly.

--- Assets/Scripts/Gameplay/GameLayers.cs
+++ Assets/Scripts/Gameplay/GameLayers.cs
@@ -8,6 +8,7 @@ public class GameLayers : MonoBehaviour
     [SerializeField] LayerMask interactableLayer;
     [SerializeField] LayerMask grassLayer;
     [SerializeField] LayerMask playerLayer;
+    [SerializeField] LayerMask fovLayer;
 
     public static GameLayers i { get; set; }
 
@@ -30,5 +31,9 @@ public class GameLayers : MonoBehaviour
 
     public LayerMask PlayerLayer {
         get => playerLayer;
+    }
+
+    public LayerMask FovLayer {
+        get => fovLayer;
     }
 }
